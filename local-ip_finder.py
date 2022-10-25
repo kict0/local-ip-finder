e1d39f385ed1a09679abdd1105cc4b3a70b6f035
kict0 <116524644+kict0@users.noreply.github.com>
2022-10-25 20:48:27 +0200

    Update local-ip-finder.py

--- local-ip_finder.py
+++ local-ip_finder.py
@@ -3,5 +3,6 @@ import socket
 hostname = socket.gethostname()
 ip = socket.gethostbyname(hostname)
 
+print("Simple local IP finder v1.0 by kict0
 print("The name of your computer is: " + hostname)
 print("Your local IP is: " + ip)
